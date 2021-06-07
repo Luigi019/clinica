9480d80adde87880fca6264a4e6b9b3c269556f8
gustavo <gasantaella@gmail.com>
2021-06-07 00:40:16 -0400

    agrgeando efecto maquina

---
+++ resources/views/layout/inc/contact.blade.php
@@ -0,0 +1,28 @@
+ <!-- Footer-->
+ <footer class="footer bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
+                        <ul class="list-inline mb-2">
+                            <li class="list-inline-item"><a href="#!">Sobre Nosotros</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="#!">Contactanos</a></li>
+                        </ul>
+                        <p class="text-muted small mb-4 mb-lg-0">&copy; {{ date('Y') }} - Centro médico Diagnóstico Clínico. All Rights Reserved.</p>
+                    </div>
+                    <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item me-4">
+                                <a href="https://www.facebook.com/Drayessiponce" target="_blank"><i class="bi-facebook fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item me-4">
+                                <a href="#!"><i class="bi-twitter fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://www.instagram.com/drayessiponc/" target="_blank"><i class="bi-instagram fs-3"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
--- resources/views/layout/inc/linksJs.blade.php
+++ resources/views/layout/inc/linksJs.blade.php
@@ -1,31 +1,4 @@
- <!-- Footer-->
- <footer class="footer bg-light">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
-                        <ul class="list-inline mb-2">
-                            <li class="list-inline-item"><a href="#!">Sobre Nosotros</a></li>
-                            <li class="list-inline-item">⋅</li>
-                            <li class="list-inline-item"><a href="#!">Contactanos</a></li>
-                        </ul>
-                        <p class="text-muted small mb-4 mb-lg-0">&copy; {{ date('Y') }} - Centro médico Diagnóstico Clínico. All Rights Reserved.</p>
-                    </div>
-                    <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
-                        <ul class="list-inline mb-0">
-                            <li class="list-inline-item me-4">
-                                <a href="https://www.facebook.com/Drayessiponce" target="_blank"><i class="bi-facebook fs-3"></i></a>
-                            </li>
-                            <li class="list-inline-item me-4">
-                                <a href="#!"><i class="bi-twitter fs-3"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="https://www.instagram.com/drayessiponc/" target="_blank"><i class="bi-instagram fs-3"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
+
 
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
@@ -36,12 +9,7 @@
 <script src="{{ asset('js/vendor/modernizr-3.11.2.min.js') }}"></script>
 <script src="{{ asset('js/plugins.js') }}"></script>
 <script src="{{ asset('js/scripts.js') }}"></script>
-
-<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-<script>
-  window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-  ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview');
-</script>
-<script src="https://www.google-analytics.com/analytics.js" async=""></script>
  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+
   @yield("js")
