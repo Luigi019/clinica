394161b2cdfb4fe81416435386cd9f6056a601a1
gustavo <gasantaella@gmail.com>
2021-06-19 19:57:33 -0400

    agregando columna cargo en el foot de la tabla

--- resources/views/admin/empleados/index.blade.php
+++ resources/views/admin/empleados/index.blade.php
@@ -25,47 +25,44 @@
         </thead>
         <tbody>
 
-          @foreach ($employees as $employee)
-          <tr>
-            <td class="text-center">{{ $employee->id }}</td>
-            <td width='16.6%' class="text-center">{{ $employee->name }}</td>
-            <td width='16.6%' class="text-center">{{ $employee->lastname }}</td>
-            <td width='16.6%' class="text-center">{{ $employee->email }}</td>
-            <td width='16.6%' class="text-center">{{ $employee->position }}</td>
-            <td width='16.6%' class="text-center"><img class="img-thumbnail img-fluid" src="{{ asset('storage'). '/' .$employee->photo}}" alt="Foto de {{ $employee->name }}" style="max-height: 150px; max-width: 150px;"></td>
-            {{-- <td width='25%'>{{ $rol->created_at }}</td>
-            <td width='25%'>{{ $rol->updated_at }}</td> --}}
-            <td class="text-center" width='16.6%'>
-              @can('Eliminar empleados')
-              <form action="{{ route('empleados.destroy',$employee->id) }}" method="GET">
-                @csrf
-                <input class="btn btn-danger font-weight-bold" type="submit" onclick="return confirm('¿Quieres eliminar este registro?')"
-                 value="Borrar">
-              </form>
-                
-              @endcan
-              @can('Editar empleados')
-              <a href="{{ route('empleados.edit',$employee->id)}}" width="33%" class="btn btn-warning font-weight-bold" title="">Editar</a>
-              @endcan
-              
-            </td>
-          </tr>
-          @endforeach
-          
-        </tbody>
+					@foreach ($employees as $employee)
+					<tr>
+						<td class="text-center">{{ $employee->id }}</td>
+						<td width='20%' class="text-center">{{ $employee->name }}</td>
+						<td width='20%' class="text-center">{{ $employee->lastname }}</td>
+						<td width='20%' class="text-center">{{ $employee->email }}</td>
+						<td width='20%' class="text-center"><img class="img-thumbnail img-fluid" src="{{ asset('storage'). '/' .$employee->photo}}" alt="Foto de {{ $employee->name }}" style="max-height: 150px; max-width: 150px;"></td>
+						{{-- <td width='25%'>{{ $rol->created_at }}</td>
+						<td width='25%'>{{ $rol->updated_at }}</td> --}}
+						<td class="text-center" width='20%'>
+							@can('Eliminar empleados')
+							<form action="{{ route('empleados.destroy',$employee->id) }}" method="GET">
+								@csrf
+								<input class="btn btn-danger font-weight-bold" type="submit" onclick="return confirm('¿Quieres eliminar este registro?')"
+								 value="Borrar">
+							</form>
+
+							@endcan
+							@can('Editar empleados')
+							<a href="{{ route('empleados.edit',$employee->id)}}" width="33%" class="btn btn-warning font-weight-bold" title="">Editar</a>
+							@endcan
+
+						</td>
+					</tr>
+					@endforeach
+
+				</tbody>
 
                 <tfoot class="thead-dark">
             <tr>
-              <th class="text-center">#</th>
-              <th class="text-center"width='16.6%'>Nombre</th>
-              <th class="text-center"width='16.6%'>Apellido</th>
-              <th class="text-center"width='16.6%'>Correo</th>
-              <th class="text-center"width='16.6%'>Cargo</th>
-              <th class="text-center"width='16.6%'>Foto</th>
-              {{-- <th class="text-center"width='25%'>Permisos</th> --}}
-              {{-- <th  class="text-center"width='25%'>Fecha de creaci&oacute;n</th>
-              <th  class="text-center"width='25%'>Fecha de actualizaci&oacute;n</th> --}}
-              <th  class="text-center"width='16.6%'>Opciones</th>
+				<th class="text-center">#</th>
+				<th class="text-center"width='20%'>Nombre</th>
+				<th class="text-center"width='20%'>Apellido</th>
+				<th class="text-center"width='20%'>Correo</th>
+				<th class="text-center"width='20%'>Cargo</th>
+				<th class="text-center"width='20%'>Foto</th>
+
+				<th  class="text-center"width='20%'>Opciones</th>
             </tr>
         </tfoot>
       </table>
